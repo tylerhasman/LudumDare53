b5e1496cc4acc366e4ff36148a0144827b7e5dc7
Tyler <tylerhasman10@hotmail.com>
2023-04-30 16:46:09 -0700

    Changed game resolution

--- desktop/src/me/ludumdare/animalgame/DesktopLauncher.java
+++ desktop/src/me/ludumdare/animalgame/DesktopLauncher.java
@@ -10,6 +10,8 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("AnimalGame");
+		config.setWindowedMode(1280, 800);
+		config.setResizable(false);
 		new Lwjgl3Application(new AnimalGame(), config);
 	}
 }
