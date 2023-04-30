7986658527b466f28020f19e2093ea6e50647f59
Tyler <tylerhasman10@hotmail.com>
2023-04-29 18:53:23 -0700

    Tower and enemy code

---
+++ core/src/me/ludumdare/animalgame/ProjectileTower.java
@@ -0,0 +1,9 @@
+package me.ludumdare.animalgame;
+
+public abstract class ProjectileTower extends Tower{
+
+    public ProjectileTower(World world) {
+        super(world);
+    }
+
+}
--- core/src/me/ludumdare/animalgame/World.java
+++ core/src/me/ludumdare/animalgame/World.java
@@ -1,8 +1,10 @@
 package me.ludumdare.animalgame;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class World {
@@ -18,8 +20,46 @@ public class World {
         toAdd.add(entity);
     }
 
-    public void update(float delta){
+    public List<Entity> getEntities() {
+        return entities;
+    }
 
+    /**
+     * Get a list of entities that are colliding with a circle.
+     * The list will be sorted from nearest to furthest.
+     * @param position the position of the circle
+     * @param radius the radius of the circle
+     */
+    public List<Entity> getCollidingEntities(Vector2 position, float radius){
+        List<Entity> colliding = new ArrayList<>();
+
+        final float radiusSquared = radius * radius;
+
+        //This is slow but can be optimized later if needed
+        for(Entity entity : entities){
+            if(entity.getPosition().dst2(position) <= radiusSquared){
+                colliding.add(entity);
+            }
+        }
+
+        colliding.sort((e1, e2) -> Float.compare(e1.getPosition().dst2(position), e2.getPosition().dst2(position)));
+
+        return colliding;
+    }
+
+    public <T extends Entity> List<T> getEntitiesOfClass(Class<? extends T> entityClass){
+        List<T> found = new ArrayList<>();
+
+        for(Entity entity : entities){
+            if(entityClass.isInstance(entity)){
+                found.add((T) entity);
+            }
+        }
+
+        return found;
+    }
+
+    public void update(float delta){
         entities.addAll(toAdd);
         toAdd.clear();
 
