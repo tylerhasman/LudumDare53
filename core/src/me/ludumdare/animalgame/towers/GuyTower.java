dd4c5402d6d0339474b857a1d2894f7f0af12bd2
nicxlr8 <nicxlr8@hotmail.com>
2023-04-30 18:25:10 -0700

    Assets towers added

--- core/src/me/ludumdare/animalgame/towers/GuyTower.java
+++ core/src/me/ludumdare/animalgame/towers/GuyTower.java
@@ -7,12 +7,12 @@ import me.ludumdare.animalgame.World;
 public class GuyTower extends ProjectileTower {
 
     public GuyTower(World world) {
-        super(world, "Old Man", "dog_idle_0", 30);
+        super(world, "Old Man", "gunguy_idle_0", 30);
     }
 
     @Override
     public Projectile createProjectile() {
-        Projectile projectile = new Projectile(getWorld(), "tennis_ball");
+        Projectile projectile = new Projectile(getWorld(), "bullet");
 
         projectile.setDamage(1);
         projectile.setRadius(16);
