e7fe92ec1105db5033ed044432ea1f9cb0a3befc
Tyler <tylerhasman10@hotmail.com>
2023-05-01 17:47:00 -0700

    Fixed wrong points

--- core/src/me/ludumdare/animalgame/Levels.java
+++ core/src/me/ludumdare/animalgame/Levels.java
@@ -83,12 +83,7 @@ public class Levels {
         LEVEL_ONE = new Level(level1Points, "level1", "level1_invalid", level1Enemies);
 
         // LEVEL 2 //////////////////////////////////////////////////////////////////////////////
-        List<Vector2> level2Points = Arrays.asList
-                (new Vector2(300, Y), new Vector2(300, Y-75),
-                        new Vector2(675, Y-75), new Vector2(675, Y-270),
-                        new Vector2(175, Y-270), new Vector2(175, Y-650),
-                        new Vector2(900, Y-650), new Vector2(900, Y-270),
-                        new Vector2(1150, Y-270), new Vector2(1150, 0));
+
 
         List<String> twoWave2 = new ArrayList<>();
         twoWave2.add(azazon);
@@ -182,6 +177,21 @@ public class Levels {
                     new Vector2(560, Y - 190), new Vector2(780, Y-160),
                     new Vector2(975, Y - 270), new Vector2(855, Y - 710),
                     new Vector2(1000, Y-755), new Vector2(1280, Y - 610));
+
+    private static         List<Vector2> level2Points = Arrays.asList
+            (new Vector2(300, Y), new Vector2(300, Y-75),
+                    new Vector2(675, Y-75), new Vector2(675, Y-270),
+                    new Vector2(175, Y-270), new Vector2(175, Y-650),
+                    new Vector2(900, Y-650), new Vector2(900, Y-270),
+                    new Vector2(1150, Y-270), new Vector2(1150, 0));
+
+    private static List<Vector2> level3Points = Arrays.asList
+            (new Vector2(0, Y-275), new Vector2(70, Y-275),
+                    new Vector2(70, Y-470), new Vector2(600, Y-470),
+                    new Vector2(600, Y-90), new Vector2(280, Y-90),
+                    new Vector2(280, Y-720), new Vector2(1030,Y-720),
+                    new Vector2(1075, Y-400), new Vector2(915, Y-200),
+                    new Vector2(1180, Y));
 
     private static final String mailman = "mail_man";
     private static final String azazon = "azazon";
@@ -345,7 +355,7 @@ public class Levels {
         level1Enemies.add(oneWave11);
         level1Enemies.add(oneWave12);
 
-        Level level = new Level(level1Points, "level2", "level2_invalid", level1Enemies);
+        Level level = new Level(level2Points, "level2", "level2_invalid", level1Enemies);
 
         LEVELS.add(level);
     }
@@ -464,7 +474,7 @@ public class Levels {
         level1Enemies.add(oneWave15);
         level1Enemies.add(oneWave16);
 
-        Level level = new Level(level1Points, "level3", "level3_invalid", level1Enemies);
+        Level level = new Level(level3Points, "level3", "level3_invalid", level1Enemies);
 
         LEVELS.add(level);
     }
